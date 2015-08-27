a9f231bf89fd2b5458a42e9b6fd6781da7a15ce7
Matthew King <mattkingit@gmail.com>
2015-08-27 17:03:35 -0400

    Offline maps - before Loading online layers

---
+++ OfflineMap/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using Esri.ArcGISRuntime.Controls;
+using Esri.ArcGISRuntime.Data;
+using Esri.ArcGISRuntime.Geometry;
+using Esri.ArcGISRuntime.Http;
+using Esri.ArcGISRuntime.Layers;
+using Esri.ArcGISRuntime.Tasks.Offline;
+
+
+namespace OfflineMap
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private const string basemapUrl = "http://sampleserver6.arcgisonline.com/arcgis/rest/services/World_Street_Map/MapServer";
+        private const string operationUrl = "http://sampleserver6.arcgisonline.com/arcgis/rest/services/Sync/SaveTheBaySync/FeatureServer/0";
+
+        private string localTileCachePath;
+        private string localGeodatabasePath;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void GetTiles(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DataOptionChecked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void GetFeatures(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
