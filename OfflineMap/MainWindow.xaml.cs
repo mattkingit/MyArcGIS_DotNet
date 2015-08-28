6a917c4417d6d86a5c26dc599142c307d670abff
Matthew King <mattkingit@gmail.com>
2015-08-28 12:53:11 -0400

    Offline Map - Download local tiles and features - working

--- OfflineMap/MainWindow.xaml.cs
+++ OfflineMap/MainWindow.xaml.cs
@@ -41,6 +41,7 @@ namespace OfflineMap
         {
             InitializeComponent();
 
+
             MyMapView.NavigationCompleted += (s, e) => { GenerateLocalTilesButton.IsEnabled = MyMapView.Scale < 6000000; };
             MyMapView.Loaded += MyMapView_Loaded;
         }
@@ -57,7 +58,7 @@ namespace OfflineMap
             {
                 // show the status controls
                 StatusPanel.Visibility = Visibility.Visible;
-                StatusMessageList.Items.Add("Requesting tile cache ...");
+                StatusMessagesList.Items.Add("Requesting tile cache ...");
 
                 // cancel if an earlier call was made
                 if (cancellationTokenSource != null)
@@ -69,12 +70,13 @@ namespace OfflineMap
                 cancellationTokenSource = new CancellationTokenSource();
                 var cancelToken = cancellationTokenSource.Token;
 
-                // create a new ExportTileCachTask to generate the tiles
+                // create a new ExportTileCacheTask to generate the tiles
                 var exportTilesTask = new ExportTileCacheTask(new Uri(basemapUrl));
 
                 //define options for the new tiles (extent, scale levels, format)
                 var generateOptions = new GenerateTileCacheParameters();
                 generateOptions.Format = ExportTileCacheFormat.CompactCache;
+                generateOptions.GeometryFilter = MyMapView.Extent;
                 generateOptions.MinScale = 6000000.0;
                 generateOptions.MaxScale = 1.0;
 
@@ -82,7 +84,7 @@ namespace OfflineMap
                 var outFolder = System.AppDomain.CurrentDomain.BaseDirectory;
                 var downloadOptions = new DownloadTileCacheParameters(outFolder);
 
-                //overwrite the file if it already exists
+                // overwrite the file if it already exists
                 downloadOptions.OverwriteExistingFiles = true;
 
                 // check generation progress every two seconds
@@ -90,53 +92,58 @@ namespace OfflineMap
 
                 var creationProgress = new Progress<ExportTileCacheJob>(p =>
                 {
-                    StatusMessageList.Items.Clear();
-                    foreach(var m in p.Messages)
+                    StatusMessagesList.Items.Clear();
+                    foreach (var m in p.Messages)
                     {
-                        // find messages with percent coplete
+                        // find messages with percent complete
                         // "Finished:: 9 percent", e.g.
                         if (m.Description.Contains("Finished::"))
                         {
                             // parse out the percentage complete and update the progress bar
                             var numString = m.Description.Substring(m.Description.IndexOf("::") + 2, 3).Trim();
                             var pct = 0.0;
-                            if (double.TryParse(numString,out pct))
+                            if (double.TryParse(numString, out pct))
                             {
                                 StatusProgressBar.Value = pct;
                             }
-                            else
-                            {
-                                // show other status message in the list
-                                StatusMessageList.Items.Add(m.Description);
-                            }
+                        }
+                        else
+                        {
+                            // show other status messages in the list
+                            StatusMessagesList.Items.Add(m.Description);
                         }
                     }
                 });
 
-                // dhow download progress
+                // show download progress 
                 var downloadProgress = new Progress<ExportTileCacheDownloadProgress>(p =>
                 {
                     StatusProgressBar.Value = p.ProgressPercentage;
                 });
 
-                // generate the tiles and download them
+                // generate the tiles and download them 
                 var result = await exportTilesTask.GenerateTileCacheAndDownloadAsync(generateOptions,
-                    downloadOptions, checkInterval, cancelToken, creationProgress, downloadProgress);
+                                                                                     downloadOptions,
+                                                                                     checkInterval,
+                                                                                     cancelToken,
+                                                                                     creationProgress,
+                                                                                     downloadProgress);
 
-                // when the task completes, store the path to the local tile cache
+                // when complete, store the path to the new local tile cache
                 this.localTileCachePath = result.OutputPath;
-                LocalDataPathTextBlock.Text = this.localTileCachePath;
+                LocalTilesPathTextBlock.Text = this.localTileCachePath;
                 LocalTilesPathTextBlock.ToolTip = this.localTileCachePath;
 
-                //clear the wroking messages, report success
+
+                // clear the working messages, report success
                 StatusProgressBar.Value = 100;
-                StatusMessageList.Items.Clear();
-                StatusMessageList.Items.Add("Local tiles created at " + this.localTileCachePath);
+                StatusMessagesList.Items.Clear();
+                StatusMessagesList.Items.Add("Local tiles created at " + this.localTileCachePath);
             }
             catch (Exception exp)
             {
-                StatusMessageList.Items.Clear();
-                StatusMessageList.Items.Add("Unable to get local tiles: " + exp.Message);
+                StatusMessagesList.Items.Clear();
+                StatusMessagesList.Items.Add("Unable to get local tiles: " + exp.Message);
             }
             finally
             {
@@ -150,16 +157,66 @@ namespace OfflineMap
 
         }
 
-        private void GetFeatures(object sender, RoutedEventArgs e)
+        private async void GetFeatures(object sender, RoutedEventArgs e)
         {
+            try
+            {
+                StatusPanel.Visibility = Visibility.Visible;
+                StatusMessagesList.Items.Add("Submitting generate geodatabase job ...");
+                StatusProgressBar.IsIndeterminate = true;
+                StatusProgressBar.IsEnabled = true;
 
+                // cancel if an erliear call was made
+                if (cancellationTokenSource != null)
+                {
+                    cancellationTokenSource.Cancel();
+                }
+
+                // get a cancellation token
+                cancellationTokenSource = new CancellationTokenSource();
+                var cancelToken = cancellationTokenSource.Token;
+
+                // create a new GeodatabaseSncTask with the uri of the feature service to pull from
+                var serverUrl = operationalUrl.Substring(0, operationalUrl.LastIndexOf('/'));
+                var uri = new Uri(serverUrl);
+                var getFeaturesTask = new GeodatabaseSyncTask(uri);
+
+                // crate parameters for the task: layers and extent to include, out spation reference and sync model
+                var layers = new List<int>(new int[1] { 0 }); //just get the first layer
+                var extent = MyMapView.Extent;
+                var getFeaturesParams = new GenerateGeodatabaseParameters(layers, extent)
+                {
+                    OutSpatialReference = MyMapView.SpatialReference,
+                    SyncModel = SyncModel.PerLayer
+                };
+
+                // check progress every two seconds
+                var checkInterval = TimeSpan.FromSeconds(2);
+                var creationProgress = new Progress<GeodatabaseStatusInfo>(p =>
+                {
+                    this.StatusMessagesList.Items.Add(DateTime.Now.ToShortTimeString() + ": " + p.Status);
+                });
+
+                // call GenerateGeodatabaseAsync, the GenerateFeautresCompleteCallback callback will execute when it's complete
+                var gdbResults = await getFeaturesTask.GenerateGeodatabaseAsync(getFeaturesParams,
+                                                                                GenerateFeatuersCompleteCallback,
+                                                                                checkInterval,
+                                                                                creationProgress,
+                                                                                cancelToken);
+
+            }
+            catch (Exception ex)
+            {
+                StatusMessagesList.Items.Add("Unable to create offline database: " + ex.Message);
+                StatusProgressBar.IsIndeterminate = false;
+            }
         }
 
         private async void TryLoadOnlineLayers()
         {
             try
             {
-                //handle a variety of possible exceptions
+                // create an online tiled map service layer, an online feature layer
                 var basemapLayer = new ArcGISTiledMapServiceLayer(new Uri(basemapUrl));
                 var operationalLayer = new FeatureLayer(new Uri(operationalUrl));
 
@@ -171,12 +228,14 @@ namespace OfflineMap
                 await operationalLayer.InitializeAsync();
 
                 // see if there was an exception when initializing the layers, if so throw an exception
-                if (basemapLayer.InitializationException != null || operationalLayer.InitializationException != null)
+                if (basemapLayer.InitializationException != null ||
+                    operationalLayer.InitializationException != null)
                 {
                     // unable to load one or more of the layers, throw an exception
                     throw new Exception("Could not initialize layers");
                 }
-                //add layers
+
+                // add layers
                 MyMapView.Map.Layers.Add(basemapLayer);
                 MyMapView.Map.Layers.Add(operationalLayer);
             }
@@ -196,6 +255,38 @@ namespace OfflineMap
                 //other problems ...
                 MessageBox.Show("Unable to load online layers: " + exp.Message, "Load Error");
             }
+        }
+
+        private async void GenerateFeatuersCompleteCallback(GeodatabaseStatusInfo statusInfo, Exception ex)
+        {
+            if(ex != null)
+            {
+                this.Dispatcher.Invoke(() => StatusMessagesList.Items.Add("An exception has occured: " + ex.Message));
+                return;
+            }
+
+            // if successful, download the generated geodatabase from the server
+            var client = new ArcGISHttpClient();
+            var geodatabaseStream = client.GetOrPostAsync(statusInfo.ResultUri, null);
+
+            // create a path for the local geodatabse
+            var outFolder = System.AppDomain.CurrentDomain.BaseDirectory;
+            var geodatabasePath = System.IO.Path.Combine(outFolder, "Wildlife.geodatabase");
+
+            await Task.Factory.StartNew(async delegate
+            {
+                using (var stream = System.IO.File.Create(geodatabasePath))
+                {
+                    await geodatabaseStream.Result.Content.CopyToAsync(stream);
+                }
+
+                this.localGeodatabasePath = geodatabasePath;
+                this.Dispatcher.Invoke(() => LocalDataPathTextBlock.Text = geodatabasePath);
+                this.Dispatcher.Invoke(() => LocalDataPathTextBlock.ToolTip = geodatabasePath);
+                this.Dispatcher.Invoke(() => StatusMessagesList.Items.Add("Features downloaded to " + geodatabasePath));
+                this.Dispatcher.Invoke(() => StatusProgressBar.IsIndeterminate = false);
+            });
+
         }
     }
 }
